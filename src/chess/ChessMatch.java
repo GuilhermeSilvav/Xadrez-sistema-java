fe25c46b45be2ca557c0998b217a4d0e38079bb4
Guilherme Silva <guilhermesilvav@gmail.com>
2024-08-25 13:06:21 -0300

    Handling exceptions and clearing screen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,21 +28,21 @@ public class ChessMatch {
 	public ChessPiece performChessMove (ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
-		validatedSourcePosition(source);
+		validateSourcePosition(source);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
 	
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
-		Piece capturePiece = board.removePiece(target);
+		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
-		return capturePiece;
+		return capturedPiece;
 	}
 	
-	private void validatedSourcePosition(Position position) {
+	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
-			throw new BoardException("Position not on the board");
+			throw new ChessException("There is no piece on source position");
 		}
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
